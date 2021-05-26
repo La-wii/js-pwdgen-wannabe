233b34e6546baf9ccc28be33adda815a9f64e8d5
La-wii <sentilavale@gmail.com>
2021-05-26 15:20:48 +0200

    prompt nome + cognome + colore preferito + alert

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var testo = prompt('Nome');
+console.log(testo);
+
+var testo = prompt('Cognome');
+console.log(testo);
+
+var testo = prompt('Colore preferito');
+console.log(testo);
+
+
+alert('nomecognomecolorepreferito21')
